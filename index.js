3ed3b86018f4a7719029297de25bc982c2d901b7
Carolina <c.floresvaldivia@hotmail.com>
2024-06-01 22:26:37 -0400

    función botones

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ precio = 400000
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+//boton para agregar//
+
+const btnmas = document.querySelector('#btnMas')
+const result = document.querySelector('.cantidad')
+let contador = 0 
+btnmas.addEventListener("click", ()=> {
+    contador = contador + 1
+    result.innerHTML = contador
+})
+
+//boton para quitar//
+
+const btnMenos = document.querySelector('#btnMenos')
+
+btnMenos.addEventListener("click", ()=>{
+    contador = contador - 1
+    result.innerHTML = contador
+})
